872317ef5491d19f1f22d73c404fa957b25dbc84
Alen767 <masis.sis.04@bk.ru>
2022-12-09 19:30:56 +0300

    kinopand

---
+++ telegram.php
@@ -0,0 +1,28 @@
+<?php
+
+/* https://api.telegram.org/botXXXXXXXXXXXXXXXXXXXXXXX/getUpdates,
+где, XXXXXXXXXXXXXXXXXXXXXXX - токен вашего бота, полученный ранее */
+
+$name = $_POST['user_name'];
+$phone = $_POST['user_phone'];
+$email = $_POST['user_email'];
+$token = "5309810356:AAHAM4PD82_cd_I6rzpomfaXS3k5fXBC948";
+$chat_id = "-773457968";
+$arr = array(
+  'Имя пользователя: ' => $name,
+//   'Телефон: ' => $phone,
+  'Сообщение:' => $email
+);
+
+foreach($arr as $key => $value) {
+  $txt .= "<b>".$key."</b> ".$value."%0A";
+};
+
+$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
+
+if ($sendToTelegram) {
+  header('Location: thank-you.html');
+} else {
+  echo "Error";
+}
+?>
